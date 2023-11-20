2f638b22e768da6dbeea4930f08b457b83e3ea7a
lun1983 <75679299+lun1983@users.noreply.github.com>
2023-11-20 11:50:38 +0800

    密码产生器

---
+++ genPassword/genPassword.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : genPassword.py
+# Time       ：20/11/2023 11:07
+# Author     ：CBIC-IT-TEAM
+# version    ：python 3.11
+# Conda Env  : base v23.9.0
+# Description：
+"""
+
+import random
+import string
+
+RESULTFILE = './PASSWORD.TXT'
+
+
+def main():
+    print('****** genPassword start *****')
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    pswd = ''.join(random.choice(characters) for _ in range(8))
+
+    print('pswd: ' + pswd)
+    with open(RESULTFILE, 'w') as file:
+        file.writelines(pswd)
+
+    return
+
+    print('****** genPassword end *****')
+
+
+if __name__ == "__main__":
+    main()
